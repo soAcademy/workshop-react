92ef487673f789f3b89b998ec32da3ea79256aef
Your name01~ <your@email.com>
2023-01-16 11:57:48 +0700

    ws3

--- src/index.js
+++ src/index.js
@@ -5,11 +5,41 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const element = <div>Hello</div>;
+const element = (
+
+  <div>
+  <h1>Hello</h1>
+  <h2>World!</h2>
+  </div>
+  )
+
+// const fullName = 'Poovares Kongkarchui';
+// const element = <div>{fullName}</div>
+
+// const element = <div>{ 1 + 1 }</div
+
+// const element = <div>{ user.firstName + user.lastName }</div>
+
+// const element = <div>{ displayName(user) }</di
+
+// const oddOrEven = (number) => {
+// if (number % 2 === 0)
+// return 'even'
+// else
+// return 'odd'
+// }
+// const element = <div>{ oddOrEven(100) }</d>
+
+// root.render(element);
+    
+
+  
+
+
+
+    
+    
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
