4b513ff7e7dbf2b10f89681d64e0a5e7528711d3
mplug <oppoalien@gmail.com>
2023-01-16 11:18:32 +0700

    feat: do React JSX workshop

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// const fullName = 'Dominic Torretto';
+const user = {
+  firstName: "Dominic",
+  lastName: "Torretto",
+};
+
+const displayName = (user) => {
+  return user.firstName + " " + user.lastName;
+};
+
+const oddOrEven = (num) => (num % 2 === 0 ? "even" : "odd");
+// const element = <div><h1>Hello</h1><h2>World!</h2></div>;
+// const element = <div>{fullName}</div>
+const element = (
+  <div>
+    {displayName(user)}
+    <div>{oddOrEven(5)}</div>
+  </div>
 );
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
