112111ac651ee9d687c405837b1bfda7f2604caa
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-16 11:44:25 +0700

    ans: JSX Workshop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const element = <div>Hello</div>;
+  const element2 = (
+    <div>
+      <h1>Hello</h1>
+      <h2>World!</h2>
     </div>
+  );
+  const element3 = <div>{1 + 1}</div>;
+  const fullName = "Khun boeing";
+  const user = {
+    firstName: "Khun",
+    lastName: "Boeing !!",
+  };
+
+  const oddOrEven = (number) => {
+    if (number % 2 === 0) return "even";
+    else return "odd";
+  };
+
+  return (
+    <>
+      <div>{element}</div>
+      <div>{element2}</div>
+      <div>{fullName}</div>
+      <div>{element3}</div>
+      <div>{user.firstName + "  " + user.lastName}</div>
+      <div>{(oddOrEven = {})}</div>
+    </>
   );
 }
 
