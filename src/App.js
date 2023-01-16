d7ccfd871007639b160c8d4fbd7178c89d83ba0f
Firstweera <firstweera357@gmail.com>
2023-01-16 11:20:31 +0700

    feat: ANS React JSX Workshop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
+        <p>
+          "Hello React!"
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const fullName = "Dominic Torretto";
+const displayName = (user) => {
+  return user.firstName + " " + user.lastName;
+};
+const user = {
+  firstName: "Dominic",
+  lastName: "Torretto",
+};
+const oddOrEven = (number) => {
+  if (number % 2 === 0) return "Even";
+  else if (number % 2 === 1) return "odd";
+};
+
+const element = (
+  <div>
+    <h1>Hello</h1>
+    <h2>World!</h2>
+    {fullName}
+    <br />
+    {1 + 1}
+    <br />
+    {user.firstName + " " + user.lastName}
+    <br />
+    {displayName(user)}
+    <br />
+    {oddOrEven(9)}
+    <br />
+    {oddOrEven(10)}
+  </div>
 );
+
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
