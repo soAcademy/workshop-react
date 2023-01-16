a7dc238adf7dd2d437a6697a80709c9a7a0880e8
Your name <your@email.com>
2023-01-16 11:54:10 +0700

    feat: complete workshop Q1 - Q7

--- src/index.js
+++ src/index.js
@@ -10,7 +10,48 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+//Q1
+const element = <div>Hello</div>;
+root.render(element);
+//Q2
+const element1 = <div>
+  <h1>Hello</h1>
+  <h2>World!</h2>
+</div>
+//Q3
+const fullName = 'Dominic Torretto'
+const element2 = <div>{fullName}</div>
+//Q4
+const element3 = <div> {1 + 1} </div>
+root.render(element3);
+//Q5
+const user = {
+  firstName: 'Dominic',
+  lastName: 'Torretto'
+}
+const element4 = <div>{user.firstName + user.lastName}</div>
+root.render(element4);
+//Q6
+const displayName = (user) => {
+  return user.firstName + ' ' + user.lastName
+}
+const user1 = {
+  firstName: 'Dominic',
+  lastName: 'Torretto'
+}
+const element5 = <div>{displayName(user1)}</div>
+root.render(element5);
+//Q7
+const oddOrEven = number => {
+  if (number % 2 !== 0) {
+    return 'odd';
+  } else {
+    return 'even';
+  }
+  
+}
+const element6 = <div>{oddOrEven(100)}</div>
+root.render(element6);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
