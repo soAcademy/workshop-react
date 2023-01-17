647e18175bcd31af771db7ef6aa1b627050ed5a1
BornDhamachart <dhamachart.c@gmail.com>
2023-01-17 14:13:22 +0700

    feat: submit workshopp2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 const App = () => {
   return (
     <>
-      <div className="text-red-700 text-3xl font-bold underline">
+      <div className="w-1/6 bg-slate-800 mt-4 p-4 rounded-md ml-4">
+        <img className="w-full rounded-md" src="iphone14.jpeg" alt="" />
+        <h1 className="text-white text-xl font-bold mt-8">
+          THE NEW : IPHONE15 2023
+        </h1>
+        <p className="text-gray-400 mt-2 text-sm">
+          lorem ............................. lorem
+          .............................lorem .............................lorem
+          .............................
+        </p>
+        <div className="text-right text-amber-600 mt-2 text-sm italic">
+          25,900 THB
+        </div>
+        <button className="bg-blue-500 text-white w-full p-2 mt-2 rounded-md text hover:bg-blue-700 active:bg-amber-500">
+          Add to cart
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
+/*
+Quiz 1
+
+<div className="text-red-700 text-3xl font-bold underline">
         Hello World
       </div>
 
@@ -20,8 +46,4 @@ const App = () => {
           Button 2
         </button>
       </div>
-    </>
-  );
-};
-
-export default App;
+*/
