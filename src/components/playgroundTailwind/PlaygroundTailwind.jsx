24204d89b0417c4e9838c9d24f7b889f1bd60726
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-17 14:21:34 +0700

    feat: workshop

--- src/components/playgroundTailwind/PlaygroundTailwind.jsx
+++ src/components/playgroundTailwind/PlaygroundTailwind.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { myImg } from "../../myInformation";
 
 function PlaygroundTailwind() {
   return (
@@ -86,6 +87,21 @@ focus:ring-violet-300 w-[250px] h-[40px] rounded-[20px] text-white"
       <button className="w-36 bg-red-200 active:bg-red-500 focus:ring focus:outline-nonefocus:ring-red-800 ">
         Button 2
       </button>
+
+      <div className="bg-gray-300 w-[300px] h-[400px] rounded-[20px] mt-[30px]">
+        <div className="w-full ml-[50px] pt-[30px] ">
+          <img className="w-[200px] h-[200px]" src={myImg.url}></img>
+        </div>
+        <div className="ml-[200px] pt-[40px] pb-[20px] space-x-6 ">
+          <span className="pr-[40px] text-[20px]"> rabbitz </span>
+          <span className="text-red-500">$0.01</span>
+        </div>
+        <div className="w-full ml-[100px]">
+          <button className="w-[100px] h-[40px] bg-gray-600 text-white hover:bg-green-500 hover:text-white rounded-[10px]">
+            buy now !
+          </button>
+        </div>
+      </div>
     </>
   );
 }
