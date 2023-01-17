806240602cd2911d00bc83a8d92416ef363fd8ea
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-17 11:11:32 +0700

    feat: padding margin

--- src/App.js
+++ src/App.js
@@ -24,10 +24,31 @@ const App = () => {
       <p className="font-light">Hello World</p>
       <p className="italic">Hello World</p>
       <p className="underline">Hello World</p>
-      <p className="text-green-300 text-center font-bold underline italic">Text Combo</p>
+      <p className="text-green-300 text-center font-bold underline italic">
+        Text Combo
+      </p>
+      <div className="bg-yellow-300">Hello World</div>
+      <div className="bg-red-300 pt-4">Hello World</div>
+      <div className="bg-green-300 mt-4">Hello World</div>
+      <div className="bg-blue-300 ml-4">Hello World</div>
+      <div className="bg-purple-300 pl-4">Hello World</div>
+      <div className="bg-amber-300 text-right mr-4">Hello World</div>
+      <div className="bg-cyan-300 text-right pr-4">Hello World</div>
+      <div className="bg-red-400 py-4">Hello World</div>
+      <div className="bg-yellow-400 pb-4">Hello World</div>
+      <div className="bg-orange-400 p-4">Hello World</div>
+      <div className="bg-green-400 m-4">Hello World</div>
+      <div className="flex bg-blue-400 space-x-16">
+        <div className="bg-red-400">01</div>
+        <div className="bg-orange-400">02</div>
+        <div className="bg-gray-400">03</div>
+      </div>
+
+
+      <div className="pb-64"></div>
     </>
   );
 };
-
+// t (top), b (bottom), l (left), r (right), y (top and bottom), x (left and right)
 export default App;
 // xs, sm, base, md, lg, xl, 2xl
