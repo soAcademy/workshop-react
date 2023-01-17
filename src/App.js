de0b4676e77374c7a85cdc1e5ef65c4207a92ab4
Phisit Siprasatthong <phisite@gmail.com>
2023-01-17 14:54:20 +0700

    fix: add header

--- src/App.js
+++ src/App.js
@@ -61,6 +61,9 @@ function App() {
     //   </button>
     // </div>
     <div>
+      <div className="bg-gray-800 text-gray-50 px-4 py-6 text-2xl font-bold text-center">
+        Welcome to Apple
+      </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 p-4 bg-gray-500">
         <ProductCard />
         <ProductCard />
