1e80604e84096a553686753cd71054340395028d
mplug <oppoalien@gmail.com>
2023-01-17 14:04:39 +0700

    feat: do tailwindcss workshop 3

--- src/App.js
+++ src/App.js
@@ -41,14 +41,40 @@ function App() {
     //   </button>
     // </div>
 
-    <div>
-      <div className="text-red-700 text-3xl font-bold underline">Hello World</div>
-      <div className="h-38 w-24 bg-blue-500">w-24</div>
-      <div className="h-38 w-1/2 bg-yellow-300 rounded-lg">w-1/2</div>
-      <button className="w-24 bg-green-200 hover:bg-green-800">Button 1</button>
-      <button className="w-36 bg-red-200 active:bg-red-500 focus:outline-none focus:ring focus:ring-red-800">
-        Button 2
-      </button>
+    //WORKSHOP 2
+    // <div>
+    //   <div className="text-red-700 text-3xl font-bold underline">Hello World</div>
+    //   <div className="h-38 w-24 bg-blue-500">w-24</div>
+    //   <div className="h-38 w-1/2 bg-yellow-300 rounded-lg">w-1/2</div>
+    //   <button className="w-24 bg-green-200 hover:bg-green-800">Button 1</button>
+    //   <button className="w-36 bg-red-200 active:bg-red-500 focus:outline-none focus:ring focus:ring-red-800">
+    //     Button 2
+    //   </button>
+    // </div>
+
+    //WORKSHOP 3
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-64 h-fit rounded rounded-md bg-slate-800 p-3">
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1501163268664-3fdf329d019f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="MacBook"
+          />
+        </div>
+        <div className="mt-4">
+          <h1 className="text-white text-xl">The New: MacBook Pro 2040</h1>
+        </div>
+        <div className="mt-3">
+          <p className="text-white text-sm">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's
+          </p>
+        </div>
+        <div className="text-right text-red-400 mt-4">$10,000</div>
+        <div className="mt-4">
+          <button className="w-full h-8 rounded rounded-md bg-blue-700 text-white hover:bg-blue-900 active:bg-blue-600">Add to Cart</button>
+        </div>
+      </div>
     </div>
   );
 }
