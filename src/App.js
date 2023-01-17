426280c2966b284d3ec4af3c9253a072f059ea62
Thanapon Bunchot <116933922+ThanaponBunchot@users.noreply.github.com>
2023-01-17 08:54:11 +0700

    feat: resume layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DewImg from "./dew2.jpg";
+import locationImg from "./location.png";
+import telImg from "./tel.png";
+import emailImg from "./email.png";
+
+const aboutme = "ABOUT ME";
+const aboutmeContent = "My name is Thanapon Bunchot...I come from Thailand.";
+const websiteLinkHeader = "WEBSITE & SOCIAL LINKS";
+const WebsiteContent = " Facebook";
+const fullname = "THANAPON BUNCHOT";
+const contactInfo = "104/7 50 Phahonyothin, Saimai, BKK    +66-93-537-2883         thanapon.bunchot@gmail.com      ";
+const workexHeader = "WORK EXPERIENCE";
+const workYear = "2022-present "
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React ! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frame">
+      <div className="left-block">
+        <div className="img">
+          <img className="imgDew" src={DewImg} />
+        </div>
+        <div className="aboutme">{aboutme}</div>
+        <div className="aboutmeBody">{aboutmeContent}</div>
+        <div className="contact">{websiteLinkHeader}</div>
+        <div className="contactBody">{WebsiteContent}</div>
+      </div>
+
+      <div className="right-block">
+        <div className="header">
+          <div className="name">
+            {fullname}
+            <div className="job">Fullstack dev</div>
+          </div>
+          <div className="tel">
+            <div className="icon">
+              < img className = 'address'src={locationImg}></img>
+              < img className = 'address'src={telImg}></img>
+              < img className = 'address3'src={emailImg}></img>
+            </div>
+            <div className="telInfo">{contactInfo}</div>
+          </div>
+        </div>
+
+        <div className="work-ex">
+          <div className="head-work-ex">{workexHeader}</div>
+          <div className="workFrame">
+            <div className="work-nav">{workYear}</div>
+            <div className="work-content">work-content</div>
+          </div>
+        </div>
+
+        <div className="education">
+          <div className="head-education-ex">Education</div>
+          <div className="educationFrame">
+            <div className="education-nav">education year</div>
+            <div className="education-content">education-content</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import header from './header';
-import './myStyle.css' 
-const myStyles = {
-  color:'blue',
-  backgroundColor:'red',
-};
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const displayName =(user)=>{
@@ -19,18 +16,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //   lastName: "Doe",
 // };
 
-const element = <div>
-    {/* {displayName(user )} */}
-    <h1 style={{color:'blue'}}>Hello Blue Header</h1>
-    <h1 style={{backgroundColor:'red'}}> Hello Red Background</h1>
-    <div style={myStyles}>Hello style</div>
-    <div className='blue-background'>Hello Style by import</div>
-    <div className='blue-with-red-kanit'>Hello Kanit font</div>
-</div>
 
 
 root.render(
-  element
+  <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
