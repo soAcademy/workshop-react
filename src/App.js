d8445ba2ea2fd1afc33bb5ec5e734ebce81da363
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-17 10:04:37 +0700

    fix: delete package-lock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 const App = () => {
-
-  return (
-    <div className="text-lg text-red-700">Hello World</div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
 };
 
 export default App;
