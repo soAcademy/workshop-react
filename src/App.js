7d501a1341c1756df377792dcf3bd0a7941f2f66
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-17 11:31:15 +0700

    feat: width

--- src/App.js
+++ src/App.js
@@ -42,6 +42,36 @@ const App = () => {
         <div className="bg-gray-400">Hello World</div>
       </div>
 
+      <div className="w-24 bg-red-700">Hello World</div>
+      <div className="w-32 bg-yellow-700">Hello World</div>
+      <div className="w-3/12 bg-blue-300">Hello World</div>
+      <div className="w-1/2 bg-green-300">Hello World</div>
+      <div className="w-1/3 bg-cyan-300">Hello World</div>
+      <div className="w-3/4 bg-amber-300">Hello World</div>
+      <div className="w-3/5 bg-yellow-300">Hello World</div>
+      <div className="w-full bg-red-300">Hello World</div>
+      <div className="bg-green-300 m-4 p-4 rounded">Hello World</div>
+      <div className="bg-red-300 m-4 p-4 rounded-lg">Hello World</div>
+      <div className="bg-yellow-300 m-4 p-4 rounded-xl">Hello World</div>
+      <div className="bg-blue-300 m-4 p-4 rounded-full">Hello World</div>
+      <div className="bg-purple-300 w-20 m-4 p-4 rounded-full">Hello World</div>
+      <div className="bg-green-300 h-8">Hello World</div>
+      <div className="bg-red-300 h-24">Hello World</div>
+      <div className="bg-blue-300 h-32">Hello World</div>
+      <div className="bg-gray-300 h-screen">Hello World</div>
+      <div className="bg-green-300 w-screen">Hello World</div>
+
+      <div className="w-64 bg-red-700 m-4">
+        Hello World
+        <div className="w-full bg-yellow-300">Hello World</div>
+        <div className="w-screen bg-green-300">Hello World</div>
+      </div>
+
+      <div className="w-64 bg-red-700 rounded-lg">
+        <div className="w-2/3 bg-green-300 rounded-full p-4">
+          Hello World
+        </div>
+      </div>
       <div className="pb-64"></div>
     </>
   );
