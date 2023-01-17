8d1e42393e03cae089561a587c9904adb112bd1b
Panasun Sunanta <panasunsunanta@gmail.com>
2023-01-17 10:47:21 +0700

    feat: add text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <>
+      <p className="text-lg">Font Size</p>
+      <p className="text-base">Hello World!</p>
+      <p className="text-xl">Hello World!</p>
+      <p className="text-3xl">Hello World!</p>
+      <p className="text-lg">Font Size</p>
+      <p className="text-red-100">Color</p>
+      <p className="text-red-500">Hello World</p>
+      <p className="text-red-900">Hello World</p>
+      <p className="text-yellow-500">Hello World</p>
+      <p className="text-primary">Hello World</p>
+      <p className="text-secondary">Hello World</p>
+      <p className="text-gray-400">Hello World</p>
+      <p className="text-black">Hello World</p>
+      <p className="text-black">Alignment</p>
+      <p className="text-center">Hello World</p>
+      <p className="text-right">Hello World</p>
+      <p className="text-left">Hello World</p>
+      <p className="text-black">FontWeight</p>
+      <p className="font-bold">Hello World</p>
+      <p className="font-semibold">Hello World</p>
+      <p className="font-light">Hello World</p>
+      <p className="italic">Hello World</p>
+      <p className="underline">Hello World</p>
+      <p className="text-green-300 text-center font-bold underline italic">Text Combo</p>
+    </>
+  );
 };
 
 export default App;
+// xs, sm, base, md, lg, xl, 2xl
