86795d2b03d400016cb8485edb7005b9edfc49b2
Phisit Siprasatthong <phisite@gmail.com>
2023-01-17 09:11:03 +0700

    feat: format layout and add basic content

--- src/App.js
+++ src/App.js
@@ -7,68 +7,22 @@ import "./tailwind.css";
 // import { Header2, Header3 } from "./Header2";
 
 function App() {
-  // const myStyle = {
-  //   color: "lightblue",
-  //   backgroundColor: "red",
-  // };
-  // const customers = [
-  //   {
-  //     name: "Bin",
-  //     orderValue: 1400,
-  //   },
-  //   {
-  //     name: "Jam",
-  //     orderValue: 1500,
-  //   },
-  //   {
-  //     name: "Ploy",
-  //     orderValue: 2000,
-  //   },
-  // ];
+  const myCV = {
+    firstName: "John",
+    lastName: "Johnson",
+  };
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Header />
-        <Header2 />
-        <Header3 />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 style={{ color: "lightblue" }}>Hello Blue Header!</h1>
-        <h1 style={{ backgroundColor: "red" }}>Hello Red Background!</h1>
-        <div style={myStyle}>Hello Style!</div>
-        <div className="my-style">Hello Style from external CSS!</div>
-        <div className={myStyleModule["my-style"]}>
-          Hello Style from CSS modules!
-        </div>
-        <table className={myStyleModule["table-style"]}>
-          <thead className={myStyleModule["table-head-style"]}>
-            <th>Name</th>
-            <th>Order Value</th>
-          </thead>
-          {customers.map((customer, index) => (
-            <tr
-              className={
-                index % 2 === 0
-                  ? myStyleModule["table-row-style-odd"]
-                  : myStyleModule["table-row-style-even"]
-              }
-            >
-              <td>{customer.name}</td>
-              <td>{customer.orderValue}</td>
-            </tr>
-          ))}
-        </table>
-      </header> */}
       <div className="flex flex-row h-screen">
-        <div className="basis-1/4 bg-indigo-500">side content</div>
-        <div className="basis-3/4">main content</div>
+        <div className="basis-1/4 bg-indigo-500 p-4">
+          <h2 className="text-xl">About Me</h2>
+        </div>
+        <div className="basis-3/4 p-4">
+          <h1 className="text-2xl">
+            {myCV.firstName} {myCV.lastName}
+          </h1>
+        </div>
       </div>
     </div>
   );
