7eeea0a690369abaf235104c4593eea5a2ca8a1c
BornDhamachart <dhamachart.c@gmail.com>
2023-01-17 10:22:22 +0700

    feat: Set up Tailwind

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
+
+reportWebVitals();
 
 // Workshop Quiz
 // Quiz 1
@@ -65,4 +63,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
