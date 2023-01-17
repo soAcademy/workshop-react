f76754d8b8cb0f2b09e35ba24f6b6d5c17e6ae79
Phisit Siprasatthong <phisite@gmail.com>
2023-01-17 09:48:43 +0700

    fix: move Tailwind directives to App.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
-// import "./App.css";
+import "./App.css";
 // import "./my-style.css";
-import "./tailwind.css";
+// import "./tailwind.css";
 // import myStyleModule from "./my-style.module.css";
 // import Header from "./Header";
 // import { Header2, Header3 } from "./Header2";
@@ -15,12 +15,12 @@ function App() {
   const { firstName, lastName } = myCV;
 
   return (
-    <div className="App">
+    <div>
       <div className="flex flex-row h-screen">
-        <div className="basis-1/4 bg-indigo-500 p-4">
+        <div className="basis-1/4 bg-indigo-500 p-4 text-left">
           <h2 className="text-xl">About Me</h2>
         </div>
-        <div className="basis-3/4 p-4">
+        <div className="basis-3/4 p-4 text-left">
           <h1 className="text-2xl">
             {firstName} {lastName}
           </h1>
