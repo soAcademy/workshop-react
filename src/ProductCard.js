16f55ed6fe55ed9509310e35f8aee4f0718f18d4
Phisit Siprasatthong <phisite@gmail.com>
2023-01-18 11:29:00 +0700

    feat: enable data transfer to ProductCard component

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,22 +1,16 @@
-import macbookImage from "./images/apple-business-computer-design-5ebcfbd7be0a9712978db57eb36df918.jpg";
+// import macbookImage from "./images/apple-business-computer-design-5ebcfbd7be0a9712978db57eb36df918.jpg";
 
-const ProductCard = () => (
+const ProductCard = ({ product }) => (
   <div className="p-4 bg-gray-800 text-gray-400 rounded-lg">
     <img
-      alt="Macbook Pro 2040"
+      alt={product.name}
       // src="https://via.placeholder.com/640x480.png?text=Macbook+Pro+2040"
-      src={macbookImage}
+      src={product.image}
     />
-    <h1 className="text-2xl text-white font-bold my-4">
-      The New Macbook Pro 2040
-    </h1>
-    <p className="my-4">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu felis
-      eget nibh lobortis congue et sed neque. Nullam hendrerit magna sed arcu
-      aliquet, in cursus sapien bibendum.
-    </p>
+    <h1 className="text-2xl text-white font-bold my-4">{product.name}</h1>
+    <p className="my-4">{product.description}</p>
     <div className="text-red-500 text-sm my-4 text-right italic">
-      THB 24,900
+      THB {product.price}
     </div>
     <button className="w-full h-8 bg-blue-900 hover:bg-blue-600 text-white">
       Add to Cart
--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,4 +1,31 @@
 import ProductCard from "./ProductCard";
+import macbookImage from "./images/apple-business-computer-design-5ebcfbd7be0a9712978db57eb36df918.jpg";
+import macMiniImage from "./images/Mac_mini_(2018)_ports.jpg";
+import iPhoneImage from "./images/pexels-geometric-photography-11525164.jpg";
+
+const products = [
+  {
+    name: "The New Macbook Pro 2040",
+    price: 24900,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu felis eget nibh lobortis congue et sed neque. Nullam hendrerit magna sed arcu aliquet, in cursus sapien bibendum.",
+    image: macbookImage,
+  },
+  {
+    name: "The New Mac Mini 2040",
+    price: 14900,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu felis eget nibh lobortis congue et sed neque. Nullam hendrerit magna sed arcu aliquet, in cursus sapien bibendum.",
+    image: macMiniImage,
+  },
+  {
+    name: "The New iPhone 50",
+    price: 34900,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu felis eget nibh lobortis congue et sed neque. Nullam hendrerit magna sed arcu aliquet, in cursus sapien bibendum.",
+    image: iPhoneImage,
+  },
+];
 
 const ProductPage = () => (
   <>
@@ -6,8 +33,8 @@ const ProductPage = () => (
       Welcome to Apple
     </div>
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 p-4 bg-gray-500">
-      {[...Array(8).keys()].map(() => (
-        <ProductCard />
+      {products.map((product) => (
+        <ProductCard product={product} />
       ))}
     </div>
   </>
