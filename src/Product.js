f5d9563cedbcaab465c329f78bc9b0d2fa2eaadf
Your name <your@email.com>
2023-01-18 22:47:47 +0700

    feat: Product update with Shop

--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,19 @@
 import logo from './logo-1.jpg'
-const Product = () => {
+import Shop from './Shop'
+const Product = ({data}) => {
   return <>
   <div className="m-4 bg-slate-600 rounded-lg ">
     <div>
-      <img src={logo} className=" w-full p-6"></img>
+      <img src={data.image} className=" w-64 h-64 object-cover p-6"></img>
     </div>
     <div className="text-slate-50 text-bold text-3xl text-center">
-      The New Iphone 14 Pro
+      {data.name}
     </div>
     <div className="text-sm text-slate-300 p-4 text-center">
       Lorem ipsum dolor sit amet, consectetur adip sed b gdfgtrhyhngfhbrgshtrusryntrumdfuhdrgdrg dfbdfhdfbdf dfbgfbgfnbdfgnfgn xfghgfbgfbdfgb
     </div>
     <div className="text-right text-lg text-red-700 pr-2 ">
-      $1099
+      {data.price}
     </div>
     <div className="flex">
       <button className="bg-blue-500 w-3/4 mx-auto p-6 mb-8 hover:bg-blue-800 active:bg-orange-500 rounded-full"  >Add To Cart</button>
