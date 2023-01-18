574984dec4d43d47b9373b46021052db323c18a2
Your name <your@email.com>
2023-01-18 22:43:34 +0700

    feat:update resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,9 +1,51 @@
+import { BiPhone } from "react-icons/bi";
+import { GoLocation } from "react-icons/go";
+import { HiOutlineMail } from "react-icons/hi";
+const Resume = () => {
+  const info = {
+    name: (
+      <>
+        Anawil
+        <br />
+        Yuktanand
+      </>
+    ),
+    location: "Location: Bangkok, Thailand",
+    tel: "0835989444",
+    email: "teak.yuktanand@gmail.com",
+    titleright1: " Work Experience and Volunteering",
+    hope: " Hope for Children",
+    ivhq: "IVHQ",
+    school: "Ban Na Rab School",
+    hopeinfo:
+      "Raised 291.19 pounds for a non-profit organisation Hope for Children",
+    hopeinfo2:
+      " Developed self-confidence and strong communication skills conveying messages to crowds of people",
+    ivhqinfo: "Volunteer in a construction project in Livingstone, Zambia",
+    ivhqinfo2:
+      " Working alongside a skilled local foreman and other volunteers to build a library for a primary school",
+    schoolinfo:
+      "Teaching English at Ban Na Rap Primary School in Nan Province, Thailand for 1 month",
+    schoolinfo2:
+      " Help planning lessons, organising academic activities and accompanying a group of students on a school trip",
+    education: "Education",
+    uni: "University of Southampton",
+    unidate: "October 2019 - June 2022",
+    unicourse: "BEng Aeronautics & Astronautics",
+    highschool: "Harrow Bangkok",
+    highschooldate: "August 2012 - June 2018",
+    unidetail:
+      " Modules involved: Individual Project, Materials and Structures, Aerothermodynamics, Applications of CFD, Propulsion, Aerodynamics, Aircraft Structural Design, Aerospace Control Systems, Introduction to Aircraft Design, Experimental Methods for Aerodynamics, Mathematics for Engineering and the Environment, Systems Design and Computing, Engineering Management & Law",
+    highschooldetail:
+      "4 International A-Level Qualifications including Physics , Mathematics , Further Mathematics, Chemistry",
+    highschooldetail2: "IELTS Academic Overall Score 7.5",
+    highschooldetail3: "9 International IGCSEs Qualifications",
+  };
 
-function App() {
   return (
     <>
-      <div className="flex w-3/4 m-5 h-[750px]">
-        <div className="w-1/3 bg-sky-900 rounded-lg shadow-lg">
+      <div className="flex flex-col sm:flex-row w-3/4 m-5 h-[750px]">
+        <div className="sm:w-1/3 bg-sky-900 rounded-lg shadow-lg">
           <div>
             <img
               src="./profilepicture.jpg"
@@ -27,118 +69,103 @@ function App() {
           <div className="text-sm text-slate-50 ml-4 mt-4">Instagram: </div>
           <div className="text-sm text-slate-50 ml-4 mt-4">Line: </div>
         </div>
-        <div className="w-2/3 bg-slate-200 mr-10 rounded-lg shadow-lg">
+        <div className="sm:w-2/3 bg-slate-200 mr-10 rounded-lg shadow-lg">
           <div className="flex">
             <div className=" w-4/5 text-5xl text-sky-800 mt-6 ml-3 font-extrabold">
-              Anawil<br></br>Yuktanand
+              {info.name}
             </div>
             <div className="bg-blue-200 rounded-lg mt-4 mr-4">
-              <div className="text-xs text-sky-700 font-semibold mt-6 mr-5 text-center">
-                Location: Bangkok, Thailand
+              <div className="text-xs text-sky-700 font-semibold mt-6 ml-5">
+                {info.location}
               </div>
-              <div className="text-xs text-sky-700 font-semibold mt-2 5r-5 text-center">
-                Tel:+66835989444
+              <div className="-mt-4 ml-1 text-sky-900 text-sm font-semibold">
+                <GoLocation />
               </div>
-              <div className="text-xs text-sky-700 font-semibold mt-2 mr-4 ml-2">
-                Email:teak.yuktanand@gmail.com
+              <div className="text-xs text-sky-700 font-semibold mt-2 ml-6">
+                {info.tel}
+              </div>
+              <div className="-mt-3 ml-1 text-sky-900 text-sm font-semibold">
+                <BiPhone />
+              </div>
+              <div className="text-xs text-sky-700 font-semibold mt-1 mr-4 ml-6">
+                {info.email}
+              </div>
+              <div className="-mt-3 ml-1 text-sky-900 text-sm font-semibold">
+                <HiOutlineMail />
               </div>
             </div>
           </div>
-          <div className=" text-2xl text-sky-800 ml-3 mt-10 font-bold">
-            Work Experience and Volunteering
+          <div className="divide-y-4 divide-teal-600 divide-dashed mr-4 ">
+            <div className="text-slate-200">..</div>
+            <div className=" text-2xl text-sky-800 ml-3 mt-4 font-bold">
+              {info.titleright1}
+            </div>
           </div>
           <div className="flex">
             <div className="w-1/3">
               <div className="text-base text-sky-600 ml-5 mt-3 font-bold">
-                Hope for Children
+                {info.hope}
               </div>
               <div className="text-xs text-sky-500 ml-5">March 2020</div>
               <div className="text-base text-sky-600 ml-5 mt-9 font-bold">
-                IVHQ
+                {info.ivhq}
               </div>
               <div className="text-xs text-sky-500 ml-5">June-July 2019</div>
               <div className="text-base text-sky-600 ml-5 mt-8 font-bold">
-                Ban Na Rab School
+                {info.school}
               </div>
               <div className="text-xs text-sky-500 ml-5">February 2019</div>
             </div>
             <div className="w-2/3">
               <div className="text-xs text-sky-500 mt-4">
                 <ul>
-                  <li className="font-bold">
-                    Raised 291.19 pounds for a non-profit organisation “Hope for
-                    Children”
-                  </li>
-                  <li>
-                    Developed self-confidence and strong communication skills
-                    conveying messages to crowds of people
-                  </li>
+                  <li className="font-bold">{info.hopeinfo}</li>
+                  <li>{info.hopeinfo2}</li>
                 </ul>
               </div>
               <div className="text-xs text-sky-500 mt-4 mr-2">
                 <ul>
-                  <li className="font-bold">
-                    Volunteer in a construction project in Livingstone, Zambia
-                  </li>
-                  <li>
-                    Working alongside a skilled local foreman and other
-                    volunteers to build a library for a primary school
-                  </li>
+                  <li className="font-bold">{info.ivhqinfo}</li>
+                  <li>{info.ivhqinfo2}</li>
                 </ul>
               </div>
               <div className="text-xs text-sky-500 mt-4 mr-2">
                 <ul>
-                  <li className="font-bold">
-                    Teaching English at Ban Na Rap Primary School in Nan
-                    Province, Thailand for 1 month
-                  </li>
-                  <li>
-                    Help planning lessons, organising academic activities and
-                    accompanying a group of students on a school trip
-                  </li>
+                  <li className="font-bold">{info.schoolinfo}</li>
+                  <li>{info.schoolinfo2}</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div className=" text-2xl text-sky-800 ml-3 mt-10 font-bold">
-            Education
+          <div className="divide-y-4 divide-teal-600 divide-dashed mr-4 ">
+            <div className="text-slate-200">..</div>
+            <div className=" text-2xl text-sky-800 ml-3 mt-2 font-bold">
+              {info.education}
+            </div>
           </div>
           <div className="flex">
             <div className="w-1/3">
               <div className="text-base text-sky-600 ml-5 mt-3 font-bold">
-                University of Southampton
+                {info.uni}
               </div>
-              <div className="text-xs text-sky-500 ml-5">
-                October 2019 - June 2022
-              </div>
-              <div className="text-xs text-sky-500 ml-5">
-                BEng Aeronautics & Astronautics
-              </div>
+              <div className="text-xs text-sky-500 ml-5">{info.unidate}</div>
+              <div className="text-xs text-sky-500 ml-5">{info.unicourse}</div>
               <div className="text-base text-sky-600 ml-5 mt-6 font-bold">
-                Harrow Bangkok
+                {info.highschool}
               </div>
               <div className="text-xs text-sky-500 ml-5">
-                August 2012 - June 2018
+                {info.highschooldate}
               </div>
             </div>
             <div className="w-2/3">
               <div className="text-xs text-sky-500 mt-4 mr-2">
-                Modules involved: Individual Project, Materials and Structures,
-                Aerothermodynamics, Applications of CFD, Propulsion,
-                Aerodynamics, Aircraft Structural Design, Aerospace Control
-                Systems, Introduction to Aircraft Design, Experimental Methods
-                for Aerodynamics, Mathematics for Engineering and the
-                Environment, Systems Design and Computing, Engineering
-                Management & Law
+                {info.unidetail}
               </div>
               <div className="text-xs text-sky-500 mt-6 mr-2">
                 <ul>
-                  <li>
-                    4 International A-Level Qualifications including Physics ,
-                    Mathematics , Further Mathematics, Chemistry
-                  </li>
-                  <li>IELTS Academic Overall Score 7.5</li>
-                  <li>9 International GCSEs Qualifications</li>
+                  <li>{info.highschooldetail}</li>
+                  <li>{info.highschooldetail2}</li>
+                  <li>{info.highschooldetail3}</li>
                 </ul>
               </div>
             </div>
@@ -147,6 +174,6 @@ function App() {
       </div>
     </>
   );
-}
+};
 
-export default App;
+export default Resume;
