02f9ed49d632ab4bac8f8dec183f2350564546f0
mplug <oppoalien@gmail.com>
2023-01-18 13:02:14 +0700

    feat: do workshop pass prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// import Workshop from "./Workshop";
+import Workshop from "./Workshop";
 // import Resume from "./resume";
-import Resume2 from "./Resume2";
+// import Resume2 from "./Resume2";
 
 function App() {
-  // return <Workshop />;
+  return <Workshop />;
   // return <Resume />;
-  return <Resume2 />;
+  // return <Resume2 />;
 }
 
 export default App;
--- src/Products.js
+++ src/Products.js
@@ -1,23 +1,19 @@
-const Product = () => {
+const Products = (prop) => {
   return (
     <div className="w-64 h-fit rounded rounded-md bg-slate-800 shadow-lg shadow-gray-500 p-3">
       <div className="">
-        <img
-          src="https://images.unsplash.com/photo-1501163268664-3fdf329d019f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="MacBook"
-        />
+        <img src={prop.data.img} alt={prop.data.name} />
       </div>
       <div className="mt-4">
-        <h1 className="text-white text-xl">The New: MacBook Pro 2040</h1>
+        <h1 className="text-white text-xl">{prop.data.name}</h1>
       </div>
       <div className="mt-3">
         <p className="font-thin text-gray-400 text-sm">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's
+          {prop.data.description}
         </p>
       </div>
       <div className="text-right mt-4">
-        <p className="text-red-400 italic">$10,000</p>
+        <p className="text-red-400 italic">{prop.data.price}</p>
       </div>
       <div className="mt-4">
         <button className="w-full h-8 rounded rounded-md bg-blue-700 text-white hover:bg-blue-900 active:bg-blue-600">
@@ -28,4 +24,4 @@ const Product = () => {
   );
 };
 
-export default Product;
+export default Products;
