a7ca7ca08bca768b30fe22dfd4095f8036319a92
Phisit Siprasatthong <phisite@gmail.com>
2023-01-18 09:23:03 +0700

    fix: replace flex layout with grid

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,10 @@
 const Resume = ({ firstName, lastName }) => (
   <>
-    <div className="flex flex-row h-screen">
-      <div className="basis-1/4 bg-indigo-500 p-4 text-left">
+    <div className="grid gap-0 grid-cols-1 md:grid-cols-3 h-screen">
+      <div className="bg-indigo-500 p-4 text-left">
         <h2 className="text-xl">About Me</h2>
       </div>
-      <div className="basis-3/4 p-4 text-left">
+      <div className="md:col-span-2 p-4 text-left">
         <h1 className="text-2xl">
           {firstName} {lastName}
         </h1>
