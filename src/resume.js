9832cb5eafcecd07dabd80d457e2ff0fa7c25896
Bond Rungrot <st122150@ait.asia>
2023-01-22 19:13:30 +0700

    init

--- src/resume.js
+++ src/resume.js
@@ -1,77 +1,144 @@
-// import { FaAngellist, FaAffiliatetheme } from "react-icons/fa";
-//ส่วนนี้ไว้ Import
+import "./index.css";
+import { GoLocation } from "react-icons/go";
+import { BiPhoneCall } from "react-icons/bi";
+import { AioutlineSafetyCertificate } from "react-icons/ai";
 
-const infomation =
-  " Bond rungrot is a student gmkdfgjfak;gdfs;lgkdasf;gkfds;agk";
-
+{
+  /*Block First*/
+}
 const Resume = () => {
+  const info = {
+    aboutMe: "Bond rungrot is a student gmkdfgjfak;gdfs;lgkdasf;gkfds;ag",
+    site1: "Facebook Link เอามาใส่",
+    site2: "linkedin link",
+    site3: "webiste link",
+    fName: "Rungrot",
+    lName: "Tatiyawongwiwat",
+    location: "Bangkok, Thailand",
+    contactInfo: "0931234567",
+    university: "Thammasat University",
+    universityDetails:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Inventore Dicta offials doloure",
+  };
+
   return (
     <>
-      <div className="flex bg-white-200 h-[1300px] w-[1300px]">
-        <div className="w-[350px] bg-cyan-900">
-          <div className="flex items-center object-cover">
+      <div className="h-100 bg-teal-600 fixed w-full shadow-md text-white">
+        RT_BOND RESUME
+      </div>
+
+
+      <div className="flex bg-white-200 h-[1200px] w-[1200px] full-centered">
+        <div className="left-section w-full md:w-2/3 bg-teal-900">
+          <div className="px-8-py-12 space-y-6">
             <img
-              className="rounded "
-              src="https://scontent-xsp1-3.xx.fbcdn.net/v/t39.30808-6/322190653_1586092615252012_4335821547147473966_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG_SGzCGsmlK_9Ab9VtBW93crBXYUD7mGhysFdhQPuYaGm8YXDrE1VA4kZ2iIqJHJKSFAy1NtLidCJJJwocOt--&_nc_ohc=jPNrN9XFDx4AX8g6Ekc&_nc_ht=scontent-xsp1-3.xx&oh=00_AfBwUHq3hUX_YMfHVWrjXy-6zE3lFp2ALxweTvfH6IEShw&oe=63CC4DB1"
-              alt="Bond Rungrot"
-            />
+              className="shadow-x10"
+              src="https://scontent.fbkk13-2.fna.fbcdn.net/v/t39.30808-6/322190653_1586092615252012_4335821547147473966_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG_SGzCGsmlK_9Ab9VtBW93crBXYUD7mGhysFdhQPuYaGm8YXDrE1VA4kZ2iIqJHJKSFAy1NtLidCJJJwocOt--&_nc_ohc=EKpa1YeVZHwAX-ekx7U&_nc_zt=23&_nc_ht=scontent.fbkk13-2.fna&oh=00_AfBCWABmfuVy2oeFlI2xGDKJTzSMVYPKLv1ziQM-o-cPng&oe=63D23C71"
+              alt="Bond Work Smartly"
+            ></img>
+            <h1 className="text-5xl text-center text-slate-50">
+              Rungrot Tatiyawongwiwat
+            </h1>
+            <h1 className="mt-6 text-2x1 font-bold text-gray-">
+              You can work from anywhere.{" "}
+              <span className="text-indigo-500">Take advantage of it.</span>
+            </h1>
           </div>
         </div>
 
 
-
         <div className="w-[950px] bg-light-500">
-        <h1 className="text-5xl text-red-700 ml-10">Experiences</h1>
 
+          <h1 className="text-5xl text-gray-700 ml-5 mt-10 font-bold">WORK EXPERIENCES</h1>
+        <div className="company-detail md:flex py-3 justify-between h-435 md:h-332 lg:h-24">
+          <div className="company w-full md:w-1/4">
+            <h1 className="text-2xl text-gray-900 ml-5 font-bold">Software Development Limited</h1>
+            <li className="text-gray-700 text-lg font-bold ml-5">2018-2020</li>
+          </div>
+          <div className="display-none md:bg-gray-700 mt-1 rounded-full w-3"></div>
+          <div className="details w-full md:w-20">
+            <p className="ml-10">
+              A junior full-stack software engineer in the team of 20.
+              Work in the team of 5-6 to deliver product on time. 
+              SofdsjfkljalkjsklfjdlkfMfew=fewlakf;ldskfl;dskgf;lerskgl;.
+            </p>
+          </div>
 
+            <h1 className="text-2xl text-gray-900 ml-5 font-bold mt-3">Software Development Limited</h1>
+            <li className="text-gray-700 text-lg font-bold ml-5">2018-2020</li>
+          </div>
+          <div className="display-none md:bg-gray-700 rounded-full w-3"></div>
+          <div className="details w-full md:w-20">
+            <p className="ml-10">
+              A junior full-stack software engineer in the team of 20.
+              Work in the team of 5-6 to deliver product on time. 
+              SofdsjfkljalkjsklfjdlkfMfew=fewlakf;ldskfl;dskgf;lerskgl;.
+            </p>
 
-        <h1 className="text-5xl text-red-700 ml-10">Skills
-              <li className="text-gray-500 text-sm ml-4">Adobe Photoshop</li>
-              <li className="text-gray-500 text-sm ml-4">Adobe Illustartor</li>
-              <li className="text-gray-500 text-sm ml-4">Adobe After Effect</li>
-              <li className="text-gray-500 text-sm ml-4">Laravel</li>
-              <li className="text-gray-500 text-sm ml-4">Django</li>
-        </h1>
-        
+            <h1 className="text-2xl text-gray-900 ml-5 font-bold mt-3">Software Development Limited</h1>
+            <li className="text-gray-700 text-lg font-bold ml-5">2018-2020</li>
+          </div>
+          <div className="display-none md:bg-gray-700 rounded-full w-3"></div>
+          <div className="details w-full md:w-20">
+            <p className="ml-10">
+              A junior full-stack software engineer in the team of 20.
+              Work in the team of 5-6 to deliver product on time. 
+              SofdsjfkljalkjsklfjdlkfMfew=fewlakf;ldskfl;dskgf;lerskgl;.
+            </p>
 
-
-
-
-
-
+            <h1 className="text-2xl text-gray-900 ml-5 font-bold mt-3">Software Development Limited</h1>
+            <li className="text-gray-700 text-lg font-bold ml-5">2018-2020</li>
+          </div>
+          <div className="display-none md:bg-gray-700 rounded-full w-3"></div>
+          <div className="details w-full md:w-20">
+            <p className="ml-10">
+              A junior full-stack software engineer in the team of 20.
+              Work in the team of 5-6 to deliver product on time. 
+              SofdsjfkljalkjsklfjdlkfMfew=fewlakf;ldskfl;dskgf;lerskgl;.
+            </p>
         </div>
 
 
+          <h1 className="text-5xl text-gray-700 ml-5 mt-5 font-bold">EDUCATION</h1>
+          <div className="education-detail md:flex py-3 justify-between h-435 md:h-332 lg:h-24">
+          <div className="education w-full md:w-1/4">
+            <h1 className="text-2xl text-gray-900 ml-5 font-bold">Nebraska University</h1>
+            <li className="text-gray-700 text-lg font-bold ml-5">2016-2020</li>
+          </div>
+          <div className="display-none md:bg-gray-700 mt-1 rounded-full w-3"></div>
+          <div className="details w-full md:w-20">
+            <p className="ml-10">
+              A Master Program in Computer Engineering and Science at the Universirty of Nebraska. 
+              A Master Program in Computer Engineering and Science at the Universirty of Nebraska.
+            </p>
+          </div>
+
+            <h1 className="text-2xl text-gray-900 ml-5 font-bold mt-3">Nebraska University</h1>
+            <li className="text-gray-700 text-lg font-bold ml-5">2020-2022</li>
+          </div>
+          <div className="display-none md:bg-gray-700 rounded-full w-3"></div>
+          <div className="details w-full md:w-20">
+            <p className="ml-10">
+              A Bachelor Program in Computer Engineering and Science at the Universirty of Nebraska. 
+              A Bachelor Program in Computer Engineering and Science at the Universirty of Nebraska.
+            </p>
+            </div>
 
 
-
-
-
+          <h1 className="text-5xl text-gray-700 ml-5 mt-5 font-bold mb-3">
+            SKILLS</h1>
+            <li className="text-gray-500 text-sm ml-4">Adobe Photoshop</li>
+            <li className="text-gray-500 text-sm ml-4">Adobe Illustartor</li>
+            <li className="text-gray-500 text-sm ml-4">Adobe After Effect</li>
+            <li className="text-gray-500 text-sm ml-4">Laravel</li>
+            <li className="text-gray-500 text-sm ml-4">Django</li>
+          
+        </div>
       </div>
     </>
   );
 };
 export default Resume;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // <div className="bg-blue-900 w-5/8 h-auto p-10">
 // <div className="flex items-center justify-center mb-10">
