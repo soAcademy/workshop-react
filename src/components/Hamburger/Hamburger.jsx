a0292c6562faf033d160ef9ce16bf99be78465b0
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-23 23:34:24 +0700

    feat: update href link

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -6,12 +6,12 @@ function Hamburger() {
       <h2 className="text-white w-fit text-xl p-2 mt-[30px] bg-gradient-to-r from-indigo-500 via-purple-500 to-gray-500 rounded-[10px] md:text-4xl md:px-6">
         MENU
       </h2>
-      <a href="#">
+      <a href="https://drive.google.com/file/d/1CddrK_XYy2agpQ1Eycc7JVdsRS9P0tf3/view">
         <h1 className="text-white p-2 bg-gradient-to-r from-cyan-500 to-blue-500  rounded-[10px] mt-[30px] md:text-3xl">
           My CV Click
         </h1>
       </a>
-      <a href="#">
+      <a href="https://boeingportfirst.vercel.app/">
         <h1 className="text-white p-2 bg-gradient-to-r from-cyan-500 to-blue-500  rounded-[10px] mt-[30px] md:text-3xl">
           My Portfolio
         </h1>
