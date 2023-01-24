7c53f1ff8b6e2f6a8ca2a5228b7b8ccee7634ffa
Firstweera <firstweera357@gmail.com>
2023-01-24 16:42:56 +0700

    feat: add AppJS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./Header";
-import { Header2, Header3 } from "./Header2";
+// import Resume from "./Resume";
+import Resume2 from "./Resume2";
 
-function App() {
+
+// import Shop from "./Shop";
+// import {
+//   Workshop1,
+//   Workshop2,
+//   Workshop3,
+//   Workshop4,
+// } from "./TailwindWorkshop.js";
+
+// function App() {
+//   return (
+//     <div>
+//       <div>
+//         <Workshop1 />
+//       </div>
+//       <br />
+//       <hr />
+//       <div>
+//         <Workshop2 />
+//       </div>
+//       <br />
+//       <hr />
+//       <br />
+//       <div>
+//         <Workshop3 />
+//       </div>
+//       <br />
+//       <hr />
+//       <div>
+//         <Workshop4 />
+//       </div>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  // const string = `string text line 1
+  // string text line 2`;
+  // const a = 10;
+  // const b = 20;
+
+  // console.log(string);
+  // console.log(`Sum of a + b is: ${a + b}`);
+  // console.log(`Sum of a + b is: ${a + b}  and Multiple of a * b is: ${a * b}`);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>"Hello React!"</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Header />
-        <Header2 />
-        <Header3 />
-      </header>
-    </div>
+    <>
+      {/* <Shop /> */}
+      <Resume2 />
+      {/* <Workshop3 /> */}
+    </>
   );
-}
+};
 
 export default App;
