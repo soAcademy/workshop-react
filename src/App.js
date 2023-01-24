33e333bc209aa58b7d46032cb490a2e35b193fad
Napatpimon Ponpathanapaisarn <napatpimon.pon@gmail.com>
2023-01-24 21:40:59 +0700

    feat: prepare for pull something

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 import ResumeBoeing from "./pages/ResumeBoeing";
-import NavComponent from "./components/NavbarComponent/NavComponent";
-import FooterComponent from "./components/FooterComponent/FooterComponent";
-import ProductCard from "./components/ProductCard/ProductCard";
-import Shop from "./Shop";
 
 function App() {
   return (
     <div>
       <ResumeBoeing />
-      {/* <Shop /> */}
     </div>
   );
 }
